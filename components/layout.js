0cc376510e3cdb369c400ea823e156169c2ba9da
ATran2000 <tran.alvinx123@gmail.com>
2023-04-06 01:24:24 -0400

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -32,8 +32,8 @@ export default function Layout({ children, home }) {
               priority
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
-              height={300}
-              width={300}
+              height={250}
+              width={250}
               alt=""
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -45,8 +45,8 @@ export default function Layout({ children, home }) {
                 priority
                 src="/images/profile.jpg"
                 className={utilStyles.borderCircle}
-                height={300}
-                width={300}
+                height={250}
+                width={250}
                 alt=""
               />
             </Link>
